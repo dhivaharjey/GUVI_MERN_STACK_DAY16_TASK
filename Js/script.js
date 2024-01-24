3f0112c7c143fedfeef31ba035fc86581f3aca20
dhivaharjey <dhivahar19399@gmail.com>
2024-01-24 21:14:01 +0530

    added icon

--- Js/script.js
+++ Js/script.js
@@ -1,17 +1,3 @@
-// let countDown = function (endTime) {
-//   if (endTime > 0) {
-//     let d = document.getElementById("countDown");
-//     d.innerHTML = endTime;
-//     setTimeout(function () {
-//       countDown(endTime - 1);
-//     }, 1000);
-//   }
-// };
-// countDown(10);
-// setTimeout(() => {
-//   document.getElementById("countDown").innerText = "Happy independance day";
-// }, 10000);
-
 document.getElementById("reTry").addEventListener("click", () => {
   let countDown = function (endTime) {
     if (endTime > 0) {
