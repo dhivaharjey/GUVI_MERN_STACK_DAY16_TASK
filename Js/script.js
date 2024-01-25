23067fa1e70c0cdcacdb1dba28238a873d694e90
dhivaharjey <dhivahar19399@gmail.com>
2024-01-25 13:03:25 +0530

    changes in script

--- Js/script.js
+++ Js/script.js
@@ -1,15 +1,52 @@
+// document.getElementById("reTry").addEventListener("click", () => {
+//   let countDown = function (endTime) {
+//     if (endTime > 0) {
+//       let d = document.getElementById("countDown");
+//       d.innerHTML = endTime;
+//       setTimeout(function () {
+//         countDown(endTime - 1);
+//       }, 1000);
+//     }
+//   };
+//   countDown(10);
+//   setTimeout(() => {
+//     document.getElementById("countDown").innerText = "Happy independance day";
+//   }, 10000);
+// });
 document.getElementById("reTry").addEventListener("click", () => {
-  let countDown = function (endTime) {
-    if (endTime > 0) {
-      let d = document.getElementById("countDown");
-      d.innerHTML = endTime;
-      setTimeout(function () {
-        countDown(endTime - 1);
-      }, 1000);
-    }
-  };
-  countDown(10);
+  let counts = 10;
+  let countDown = document.getElementById("display");
   setTimeout(() => {
-    document.getElementById("countDown").innerText = "Happy independance day";
-  }, 10000);
+    countDown.innerHTML = counts--;
+    setTimeout(() => {
+      countDown.innerHTML = counts--;
+      setTimeout(() => {
+        countDown.innerHTML = counts--;
+        setTimeout(() => {
+          countDown.innerHTML = counts--;
+          setTimeout(() => {
+            countDown.innerHTML = counts--;
+            setTimeout(() => {
+              countDown.innerHTML = counts--;
+              setTimeout(() => {
+                countDown.innerHTML = counts--;
+                setTimeout(() => {
+                  countDown.innerHTML = counts--;
+                  setTimeout(() => {
+                    countDown.innerHTML = counts--;
+                    setTimeout(() => {
+                      countDown.innerHTML = counts--;
+                      setTimeout(() => {
+                        countDown.innerHTML = "Happy Independace  Day";
+                      }, 1000);
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
 });
